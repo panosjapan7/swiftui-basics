ba811126acd0b31ec06c49a27b01ca00ec654b5a
panosjapan7 <panosjapan@gmail.com>
2023-08-21 09:12:01 +0200

    41. How to use Slider in SwiftUI

---
+++ SwiftfulThinkingBootcamp/SliderBootcamp.swift
@@ -0,0 +1,52 @@
+//  SliderBootcamp.swift
+
+import SwiftUI
+
+struct SliderBootcamp: View {
+    
+    @State var sliderValue: Double = 3
+    @State var color: Color = .red
+    
+    var body: some View {
+        VStack {
+            
+            Text("Rating:")
+            Text(
+//                "\(sliderValue)"
+                
+                // formats the number to 2 decimals
+//                String(format: "%.2f", sliderValue)
+                
+                // formats the number to 0 decimals
+                String(format: "%.0f", sliderValue)
+            )
+            .foregroundColor(color)
+//            Slider(value: $sliderValue)
+//            Slider(value: $sliderValue, in: 1...5)
+//            Slider(value: $sliderValue, in: 1...5, step: 1.0)
+            Slider(
+                value: $sliderValue,
+                in: 1...5,
+                step: 1.0,
+                onEditingChanged: { (_) in
+                    color = .green
+                },
+                minimumValueLabel:
+                    Text("1")
+                    .font(.largeTitle)
+                    .foregroundColor(.orange)
+                ,
+                maximumValueLabel: Text("5"),
+                label: {
+                    Text("Title")
+                })
+                .accentColor(.red)
+        }
+    }
+}
+
+struct SliderBootcamp_Previews: PreviewProvider {
+    static var previews: some View {
+        SliderBootcamp()
+    }
+}
