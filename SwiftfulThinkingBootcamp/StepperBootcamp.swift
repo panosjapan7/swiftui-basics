44624f8915a618f8af67014b88569ae8efd57cda
panosjapan7 <panosjapan@gmail.com>
2023-08-21 08:49:15 +0200

    Create a Stepper #41

---
+++ SwiftfulThinkingBootcamp/StepperBootcamp.swift
@@ -0,0 +1,40 @@
+//
+//  StepperBootcamp.swift
+
+import SwiftUI
+
+struct StepperBootcamp: View {
+    
+    @State var stepperValue: Int = 10
+    @State var widthIncrement: CGFloat = 0
+    
+    var body: some View {
+        VStack {
+            Stepper("Stepper: \(stepperValue)", value: $stepperValue)
+                .padding(50)
+            
+            RoundedRectangle(cornerRadius: 25)
+                .frame(width: 100 + widthIncrement, height: 100)
+            
+            Stepper("Stepper 2") {
+                // increment
+                incrementWidth(amount: 10)
+            } onDecrement: {
+                // decrement
+                incrementWidth(amount: -10)
+            }
+        }
+    }
+    
+    func incrementWidth(amount: CGFloat) {
+        withAnimation(.easeInOut) {
+            widthIncrement += amount
+        }
+    }
+}
+
+struct StepperBootcamp_Previews: PreviewProvider {
+    static var previews: some View {
+        StepperBootcamp()
+    }
+}
