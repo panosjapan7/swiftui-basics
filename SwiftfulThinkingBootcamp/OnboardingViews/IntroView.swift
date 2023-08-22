17cd9c096bad62aab71e4fff804f4597bab35354
panosjapan7 <panosjapan@gmail.com>
2023-08-22 19:47:10 +0200

    53. Manage user onboarding with @AppStorage and Transitions in SwiftUI

---
+++ SwiftfulThinkingBootcamp/OnboardingViews/IntroView.swift
@@ -0,0 +1,31 @@
+//  IntroView.swift
+
+import SwiftUI
+
+struct IntroView: View {
+    
+    @AppStorage("signed_in") var currentUserSignedIn: Bool = false
+    
+    var body: some View {
+        ZStack {
+            // background
+            RadialGradient(colors: [Color.purple, Color.blue], center: .topLeading, startRadius: 5, endRadius: UIScreen.main.bounds.height)
+                .ignoresSafeArea()
+            
+            if currentUserSignedIn {
+                ProfileView()
+                    .transition(.asymmetric(insertion: .move(edge: .bottom), removal: .move(edge: .top)))
+            }
+            else {
+                OnboardingView()
+                    .transition(.asymmetric(insertion: .move(edge: .top), removal: .move(edge: .bottom)))
+            }
+        }
+    }
+}
+
+struct IntroView_Previews: PreviewProvider {
+    static var previews: some View {
+        IntroView()
+    }
+}
