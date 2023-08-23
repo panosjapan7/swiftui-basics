f490d7767a2cf19c674af968877f6de09b71850e
panosjapan7 <panosjapan@gmail.com>
2023-08-23 09:45:20 +0200

    57. Button styles, border shapes, and control sizes in SwiftUI on iOS 15

---
+++ SwiftfulThinkingBootcamp/ButtonStylesBootcamp.swift
@@ -0,0 +1,64 @@
+//  ButtonStylesBootcamp.swift
+
+import SwiftUI
+
+struct ButtonStylesBootcamp: View {
+    var body: some View {
+        VStack {
+            
+            Button {
+                
+            } label: {
+                Text("Button Title")
+                    .frame(height: 55)
+                    .frame(maxWidth: .infinity)
+            }
+            .buttonStyle(.borderedProminent)
+            .buttonBorderShape(.roundedRectangle(radius: 20))
+            .controlSize(.large)
+            
+            Button("Button Title") {
+                
+            }
+            .frame(height: 55)
+            .frame(maxWidth: .infinity)
+            .controlSize(.large)
+//            .buttonStyle(.plain)
+            .buttonStyle(.borderedProminent)
+            
+            Button("Button Title") {
+                
+            }
+            .frame(height: 55)
+            .frame(maxWidth: .infinity)
+            .controlSize(.regular)
+//            .buttonStyle(.bordered)
+            .buttonStyle(.borderedProminent)
+            
+            Button("Button Title") {
+                
+            }
+            .frame(height: 55)
+            .frame(maxWidth: .infinity)
+            .controlSize(.small)
+            .buttonStyle(.borderedProminent)
+            
+            Button("Button Title") {
+                
+            }
+            .frame(height: 55)
+            .frame(maxWidth: .infinity)
+            .controlSize(.mini)
+//            .buttonStyle(.borderless)
+            .buttonStyle(.borderedProminent)
+        }
+        .padding()
+    }
+}
+
+struct ButtonStylesBootcamp_Previews: PreviewProvider {
+    static var previews: some View {
+        ButtonStylesBootcamp()
+            
+    }
+}
