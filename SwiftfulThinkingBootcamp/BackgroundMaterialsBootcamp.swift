1bab46a1b510a5020b5220a49cf92243e68dd4d8
panosjapan7 <panosjapan@gmail.com>
2023-08-23 09:07:41 +0200

    55. System Materials and Backgrounds in iOS 15 for SwiftUI

---
+++ SwiftfulThinkingBootcamp/BackgroundMaterialsBootcamp.swift
@@ -0,0 +1,33 @@
+//  BackgroundMaterialsBootcamp.swift
+
+import SwiftUI
+
+struct BackgroundMaterialsBootcamp: View {
+    var body: some View {
+        VStack {
+            Spacer()
+            
+            VStack {
+                    RoundedRectangle(cornerRadius: 4)
+                    .frame(width: 50, height: 4)
+                    .padding()
+                Spacer()
+            }
+            .frame(height: 350)
+            .frame(maxWidth: .infinity)
+            .background(.ultraThinMaterial)
+            .cornerRadius(30)
+        }
+        .ignoresSafeArea()
+        
+        .background(
+            Image("fund")
+        )
+    }
+}
+
+struct BackgroundMaterialsBootcamp_Previews: PreviewProvider {
+    static var previews: some View {
+        BackgroundMaterialsBootcamp()
+    }
+}
